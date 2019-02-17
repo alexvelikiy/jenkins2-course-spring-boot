8d2a0692a21d9f2eab659e242232cdbfcb126a73
Alexander Velikiy <alexvelikiy@gmail.com>
2019-02-17 11:51:18 -0800

    changed heartbeat value from 10 to 11

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
@@ -52,7 +52,7 @@ public class SampleAtmosphereApplication {
 				new AtmosphereServlet(), "/chat/*");
 		registration.addInitParameter("org.atmosphere.cpr.packages", "sample");
 		registration.addInitParameter("org.atmosphere.interceptor.HeartbeatInterceptor"
-				+ ".clientHeartbeatFrequencyInSeconds", "10");
+				+ ".clientHeartbeatFrequencyInSeconds", "11");
 		registration.setLoadOnStartup(0);
 		// Need to occur before the EmbeddedAtmosphereInitializer
 		registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
